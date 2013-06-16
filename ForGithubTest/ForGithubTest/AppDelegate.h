ebc12ac1db5814b43fbab6ae14b9921881c220cb
coolthor <coolthor@gmail.com>
2013-06-16 22:47:58 +0800

    GitHub1stTest

---
+++ ForGithubTest/ForGithubTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ForGithubTest
+//
+//  Created by Thor Lin on 13/6/16.
+//  Copyright (c) 2013年 Thor Lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ForGithubTest/ForGithubTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ForGithubTest
+//
+//  Created by Thor Lin on 13/6/16.
+//  Copyright (c) 2013年 Thor Lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
